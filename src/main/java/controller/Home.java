7bf764b6acbbf1c224015fb9bc5d08ab15e2ed9f
nokiddig <sy2000dn0@gmail.com>
2022-11-20 18:13:25 +0700

    first request

--- src/main/java/controller/Home.java
+++ src/main/java/controller/Home.java
@@ -2,11 +2,18 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Home {
     @GetMapping("/home")
-    public String getHome() {
-        return "home";
+    public ModelAndView getHome() {
+        ModelAndView modelAndView = new ModelAndView("/home");
+        modelAndView.addObject("sy", 1);
+        return modelAndView;
+    }
+    @GetMapping("/")
+    public ModelAndView showHome() {
+        return new ModelAndView("/home");
     }
 }
